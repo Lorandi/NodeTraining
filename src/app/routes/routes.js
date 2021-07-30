aa36177a0134135d11b4dc0752cb6a40ec3872d0
Rodrigo Lorandi <rodrigo_lorandi@hotmail.com>
2021-07-30 20:06:04 -0300

    first commit

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -1,40 +1,62 @@
-const proResult = require("../proResult");
-const express = require('express')
-const path = require('path');
 
-const server = (server) =>{
-  server.get("/", (req, res) => {
-    res.send(`
-   <html>
-    <title>Server</title>
-      <head>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1> Chegueio </h1>
-      </body>
-    </html>
-  `);
-  });
-};
+const LivroDao = require("../infra/livro-dao")
+const db = require("../../config/database");
 
 const app = (app) => {
-  app.use(express.json())
-
-  // app.get("/", (req, res) => {
-  //   res.send("Pro Portal");
-  // });
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '../../../public/index.html'))
+  app.get('/livros', function (req, resp) {
+    const livroDao = new LivroDao(db);
+    livroDao.lista()
+      .then(livros => resp.marko(
+        require('../views/livros/lista/lista.marko'),
+        {
+          livros: livros
+        }
+      ))
+      .catch(err => console.error(err))
   });
-  app.post("/", (req, res) => {
-    const pro = req.body;
-    console.log(pro);
-    res.json(proResult(pro));
+  app.get("/", (req, resp) => {
+    resp.marko(require("../views/index/index.marko"));
+  });
+  app.get('/livros/form', function (req, resp) {
+    resp.marko(require('../views/livros/form/form.marko'),{livro: {}})
+  });
+
+  app.post('/livros', function (req, resp) {
+    console.log(req.body);
+    const livroDao = new LivroDao(db);
+    livroDao.adiciona(req.body)
+      .then(resp.redirect('/livros'))
+      .catch(erro => console.log(erro));
+  });
+
+  app.put('/livros', function (req, resp) {
+    console.log(req.body);
+    const livroDao = new LivroDao(db);
+    livroDao.atualiza(req.body)
+      .then(resp.redirect('/livros'))
+      .catch(erro => console.log(erro));
+  });
+
+  app.delete('/livros/:id', function (req, resp) {
+    const id = req.params.id;
+    const livroDao = new LivroDao(db);
+    livroDao.remove(id)
+      .then(() => resp.status(200).end())
+      .catch(erro => console.log(erro));
+  });
+  app.get('/livros/form/:id', function (req, resp) {
+    const id = req.params.id;
+    const livroDao = new LivroDao(db);
+
+    livroDao.buscaPorId(id)
+      .then(livro =>
+        resp.marko(
+          require('../views/livros/form/form.marko'),
+          { livro: livro }
+        )
+      )
+      .catch(erro => console.log(erro));
   });
 };
 
-module.exports = {
-  server,
-  app,
-};
+module.exports = app
--- src/config/custon-express.js
+++ src/config/custon-express.js
@@ -1,19 +1,23 @@
 require('marko/node-require').install();
 require('marko/express')
+const methodOverride = require('method-override');
 
 const express = require('express')
 const app = express()
-const server = express()
-const routes = require('../app/routes/routes')
 
-
-
-routes.app(app)
-routes.server(server)
-
+app.use('/estatico', express.static('src/app/public'));
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-module.exports ={
-  app,
-  server
-}
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}))
+
+const routes = require('../app/routes/routes')
+routes(app)
+
+module.exports = app
